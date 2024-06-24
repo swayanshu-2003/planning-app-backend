7da2dbf866b1329124ba5adf765381ba539f0ea3
Swayanshu Sekhar <62717967+swayanshu-2003@users.noreply.github.com>
2024-06-24 18:13:46 +0530

    Delete dist directory

--- dist/server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// import { app } from "./index";
-// app.listen(4000, (err: any) => {
-//     if (err) {
-//         console.log(err);
-//         return;
-//     }
-//     console.log(`Server running at port ${4000}`);
-// });
